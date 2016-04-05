ce55b61b76175c66a293c9737a8b676b532517a1
devo8604 <devo8604@gmail.com>
2016-04-04 20:43:45 -0600

    boostrap update

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,13 +6,17 @@
 #
 
 # Link machine www dir to vagrant home
-sudo apt-get update
-sudo apt-get install -y apache2
 
-sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt php5-curl php5-gd
-sudo a2enmod php5
-sudo a2enmod rewrite
+echo "Updating System..."
+sudo apt-get update > /dev/null
+sudo apt-get install -y apache2 > /dev/null
 
+echo "Installing Web Development Environment..."
+sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt php5-curl php5-gd > /dev/null
+sudo a2enmod php5 > /dev/null
+sudo a2enmod rewrite > /dev/null
+
+echo "Setting Configurations..."
 cat >> /etc/apache2/apache2.conf << EOF
 <Directory /var/www/html/>
         Options Indexes FollowSymLinks
@@ -22,3 +26,4 @@ cat >> /etc/apache2/apache2.conf << EOF
 EOF
 
 sudo service apache2 restart
+rm -f /var/www/html/index.hmtl
